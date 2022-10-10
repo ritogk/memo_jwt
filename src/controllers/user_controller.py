682e16edc1e2213371b217996b4272a76d0137af
Your Name <you@example.com>
2022-10-10 10:43:37 +0900

    jwt samplesakuseig

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,7 +1,8 @@
-from flask import request, redirect, url_for
+from flask import request, redirect, jsonify, url_for, make_response
 from controllers.authentication_controller.forms import UserLoginForm
 from db.db import db
 from db.models.all import User
+import jwt
 from service.UserService import UserService
 user_service = UserService()
 
@@ -14,11 +15,29 @@ class UserController:
         password = request.json["password"]
         user = user_service.create_user(name, email, username, password)
         
-        return {
+        # jwt生成
+        key = "secret"
+        content = {}
+        content["id"] = user.id
+        token = jwt.encode(content, key, algorithm="HS256").decode('utf-8')
+        # result = jwt.decode(encoded, key, algorithms="HS256")
+        # print(result)
+        
+        server_domain = 'server.test.com'
+        
+        response = make_response({
             'id': user.id,
             'name': user.name,
             'email': user.email
-        }
+        })
+        
+        response.headers.set('Content-Type', 'application/json')
+        response.headers.add('X-Content-Type-Options', 'nosniff')
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        response.set_cookie("token", value=token,
+                        httponly=True, samesite=None,
+                        domain=server_domain, path='/')
+        return response
         
 
     # ログアウト
